3bfe0f7a8491ab6e809ee64adcf8d1ba2cb43bc4
Tyler Tarsi <tytarsi@gmail.com>
2022-02-25 15:29:09 -0500

    add alacritty

--- link.sh
+++ link.sh
@@ -1,6 +1,7 @@
 CURRENT_DIRECTORY=$(pwd)
 
-ln -fhs $CURRENT_DIRECTORY/vimrc $HOME/.vimrc
-ln -fhs $CURRENT_DIRECTORY/zshrc $HOME/.zshrc
-ln -fhs $CURRENT_DIRECTORY/tmux.conf $HOME/.tmux.conf
+ln -fhs $CURRENT_DIRECTORY/.vimrc $HOME/.vimrc
+ln -fhs $CURRENT_DIRECTORY/.zshrc $HOME/.zshrc
+ln -fhs $CURRENT_DIRECTORY/.tmux.conf $HOME/.tmux.conf
+ln -fhs $CURRENT_DIRECTORY/.alacritty.yml $HOME/.alacritty.yml
 
